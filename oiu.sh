a6ed90c45d3c527a23ee7c5d267a09d5a28ef87f
yourr_email <yourr_email@example.com>
2024-01-12 13:48:22 +0800

    update

--- oiu.sh
+++ oiu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sh build.sh
 cp 系统工程师实战.html ../xiangyouzhuan.github.io/index.html
-cp -r images ../xiangyouzhuan.github.io/images
+cp -r images ../xiangyouzhuan.github.io
 git add .
 git commit -m 'update'
 git push
